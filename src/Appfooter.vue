8b0fd3207d8c68433e1191a80b5a906c9ffb6f28
alex2000d <demarcoalessandro777@gmail.com>
2024-07-29 22:58:13 +0200

    [UPD] added the list footer

--- src/Appfooter.vue
+++ src/Appfooter.vue
@@ -8,7 +8,56 @@ export default {
         <div class="container">
             <div class="row">
                 <div class="col-6 d-flex py-3">
-                    
+                    <div class="list">
+                        <div class="content">
+                            <h3 class="text-light">DC COMICS</h3>
+                            <ul class="text-secondary list-unstyled">
+                                <li>Characters</li>
+                                <li>Comics</li>
+                                <li>Movies</li>
+                                <li>TV</li>
+                                <li>Games</li>
+                                <li>Videos</li>
+                                <li>News</li>
+                            </ul>
+                        </div>
+                        <div class="content">
+                            <h3 class="text-light">SHOP</h3>
+                            <ul class="text-secondary list-unstyled">
+                                <li>Shop DC</li>
+                                <li>Shop DC Collectibles</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="list">
+                        <div class="content">
+                            <h3 class="text-light">DC</h3>
+                            <ul class="text-secondary list-unstyled">
+                                <li>Terms Of Use</li>
+                                <li>Privacy policy(New)</li>
+                                <li>Ad Choices</li>
+                                <li>Jobs</li>
+                                <li>Subscriptions</li>
+                                <li>Talent Workshops</li>
+                                <li>CPSC Certificates</li>
+                                <li>Ratings</li>
+                                <li>Shop Help</li>
+                                <li>Contact us</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="list">
+                        <div class="content">
+                            <h3 class="text-light">SITES</h3>
+                            <ul class="text-secondary list-unstyled">
+                                <li>DC</li>
+                                <li>MAD Magazine</li>
+                                <li>DC Kids</li>
+                                <li>DC Universe</li>
+                                <li>DC Power Visa</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
                 <div class="col-6 logo-container">
                     <div class="content">
