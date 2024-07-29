35bb132ad4265bcaf9e60acf55c9c1300368e366
alex2000d <demarcoalessandro777@gmail.com>
2024-07-29 19:17:01 +0200

    [UPD] added the main

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script>
 import Appheader from './Appheader.vue';
+import Appmain from './Appmain.vue';
+
 
 export default {
   components:{
-    Appheader
+    Appheader,
+    Appmain
   }
 }
 </script>
 <template lang="">
   <div>
     <Appheader />
+    <Appmain />
   </div>
 </template>
 <style lang="scss">
---
+++ src/Appmain.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+    
+}
+</script>
+<template lang="">
+    <main>
+        <div class="container-fluid ">
+            <div class="row">
+                <div class="col-12 bg-dark">
+                     <p class="text-white p-4 fs-5"><strong>_ _>content goes here_ _<</strong></p>
+                </div> 
+            </div>
+            <div class="row">
+                <div class="col-12 bg-primary p-5 text-center">
+                    <img src="../src/assets/img/buy-comics-digital-comics.png" />
+                    <span class="text-white"> digital comics</span>
+                    <img src="../src/assets/img/buy-comics-merchandise.png" />
+                    <span class="text-white"> dc merchandise</span>
+                    <img src="../src/assets/img/buy-comics-subscriptions.png" />
+                    <span class="text-white"> subscription</span>
+                    <img src="../src/assets/img/buy-comics-shop-locator.png" />
+                    <span class="text-white"> comic shop locator</span>
+                    <img src="../src/assets/img/buy-dc-power-visa.svg" />
+                    <span class="text-white"> dc power visa</span>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+<style lang="scss">
+    p{
+        margin-left: 120px;
+    }
+    img{
+        max-width: 40px;
+    }
+
+    span{
+        padding-right: 90px;
+    }
+</style>
