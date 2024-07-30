59a644cfab41713faf74f64be1b546d51c480940
alex2000d <demarcoalessandro777@gmail.com>
2024-07-30 18:27:40 +0200

    [UPD] added the image cycled from the array and exercise complete

--- src/Appmain.vue
+++ src/Appmain.vue
@@ -16,64 +16,64 @@ export default {
                type: "comic book"
           },
           {
-               "thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
-               "price": "$16.99",
-               "series": "Aquaman",
-               "type": "graphic novel"
+               thumb: "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+               price: "$16.99",
+               series: "Aquaman",
+               type: "graphic novel"
           },
           {
-               "thumb": "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
-               "price": "$2.99",
-               "series": "Batgirl",
-               "type": "comic book"
+               thumb: "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+               price: "$2.99",
+               series: "Batgirl",
+               type: "comic book"
           },
           {
-               "thumb": "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
-               "price": "$3.99",
-               "series": "Batman",
-               "type": "comic book"
+               thumb: "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+               price: "$3.99",
+               series: "Batman",
+               type: "comic book"
           },
           {
-            "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
-            "price": "$2.99",
-            "series": "Batman Beyond",
-            "type": "comic book"
+              thumb: "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
+              price: "$2.99",
+              series: "Batman Beyond",
+              type: "comic book"
           },
           {
-            "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
-            "price": "$3.99",
-            "series": "Batman/Superman",
-            "type": "comic book"
+              thumb: "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
+              price: "$3.99",
+              series: "Batman/Superman",
+              type: "comic book"
            },
            {
-                "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
-                "price": "$4.99",
-                "series": "Batman/Superman Annual",
-                "type": "comic book"
+               thumb: "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
+                price: "$4.99",
+                series: "Batman/Superman Annual",
+                type: "comic book"
            },
            {
-                "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
-                "price": "$5.99",
-                "series": "Batman: The Joker War Zone",
-                "type": "comic book"
+                thumb: "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
+                price: "$5.99",
+                series: "Batman: The Joker War Zone",
+                type: "comic book"
            },
            {
-                  "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
-                  "price": "$6.99",
-                  "series": "Batman: Three Jokers",
-                  "type": "comic book"
+                thumb: "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
+                price: "$6.99",
+                series: "Batman: Three Jokers",
+                type: "comic book"
            },
            {
-                  "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
-                  "price": "$4.99",
-                  "series": "Batman: White Knight Presents: Harley Quinn",
-                  "type": "comic book"
+                thumb: "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
+                price: "$4.99",
+                series: "Batman: White Knight Presents: Harley Quinn",
+                type: "comic book"
            },
            {
-                  "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
-                  "price": "$16.99",
-                  "series": "Catwoman",
-                  "type": "graphic novel"
+                thumb: "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
+                price: "$16.99",
+                series: "Catwoman",
+                type: "graphic novel"
            }
           ],
         }  
@@ -82,19 +82,22 @@ export default {
 </script>
 <template lang="">
         <div class="jumbotron">
-            <img src="../src/assets/img/jumbotron.jpg" />
+            <span class="badge text-bg-primary rounded-0 px-4 py-2">
+                <h3>CURRENT SERIES</h3>
+            </span>
         </div>
     <div class="bg-dark">
         <div class="container">
-            <div class="row">
-                <div class="col-12 col-md-3" v-for="pasta, index in pastas" :key="index">
-                    <div class="card m-3" style="width: 18rem;">
-                        <img src="../src/assets/img/jumbotron.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                          <p class="card-text"></p>
-                        </div>
-                      </div>
+            <div class="row pt-5">
+                <div class="col-2 " v-for="comic, index in comics" :key="index">
+                    <div class="m-2">
+                        <img class="image" :src="comic.thumb" :alt="comic.series">
+                        <p class="text-white">{{comic.series}}</P>
+                    </div>
                 </div> 
+                <div class="text-center py-2">
+                    <button class="btn btn-primary rounded-0 px-5">LOAD MORE</button>
+                </div>
             </div>
         </div>
     </div>
@@ -134,13 +137,24 @@ export default {
         overflow: hidden;
     }
     
-    .jumbotron img{
-         width: 100%;
-         max-height: 350px;
-         object-fit: cover;
-         object-position: top;
-        
-        
+    .jumbotron{
+         background-image: url('../src/assets/img/jumbotron.jpg');
+         background-size: cover;
+         height: 300px;
+         position: relative;
+
+         .badge{
+            position: absolute;
+            bottom: -25px;
+            left: 90px;
+         }
+    }
+
+    
+
+    .image{
+        height: 250px;
+        width: 100%;
     }
 
     
